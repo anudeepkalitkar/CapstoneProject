8209ef6607c414fac1b2096988025bfc9aba135e
Anudeep kalitkar <anudeep.kalitkar@gmail.com>
2021-10-26 08:48:31 +0530

    clean

--- MileStone1/KafkaConsumer.py
+++
@@ -1,7 +0,0 @@
-from mongoInjection import InjectToMongodb
-from kafka import KafkaConsumer
-from json import loads
-consumer = KafkaConsumer('newsarticles', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=True, group_id='my-group', value_deserializer=lambda x: loads(x.decode('utf-8')))
-for message in consumer:
-    InjectToMongodb(message.value)
-    print(len(message.value), type(message.value))
