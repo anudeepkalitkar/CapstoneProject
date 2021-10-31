ed4f174e02d88185eed307f42fdb3904c1b2be79
Anudeep kalitkar <anudeep.kalitkar@gmail.com>
2021-10-31 22:45:26 +0530

    dockerized

--- Producer/KafkaProducer.py
+++ Producer/KafkaProducer.py
@@ -3,6 +3,7 @@ from RssFeed import GetRssFeedUrls, GetRssFeedData
 from json import dumps
 from kafka import KafkaProducer
 import time
+
 rssSources=[
     {'name': 'TOI','homePage': 'https://timesofindia.indiatimes.com/',
     'rssPage': 'https://timesofindia.indiatimes.com/rss.cms','rssLinkFilter' : 'rssfeed'},
@@ -11,7 +12,6 @@ rssSources=[
     'rssPage': 'https://www.ndtv.com/rss','rssLinkFilter' : 'feedburner'}
     ]
 queryStringList = ["Elon Musk","Bitcoin", "Rohit Sharma", "Harry Potter", "Egyptian Pyramids", "Covid", "Fifa", "Donald Trump", "Engineering", "Bank Robbery"]
-
 def produceData(sourceType, sourceParams):
     producer = KafkaProducer(bootstrap_servers=['kafka:29092'],value_serializer=lambda x: dumps(x).encode('utf-8'))
 
@@ -28,8 +28,11 @@ def produceData(sourceType, sourceParams):
                 producer.send(topic = "newsarticles", value = articles)
 
 for t in range(10):
-    # produceData('rss',rssSources)
-    produceData('rapid',queryStringList)
+    try:
+        produceData('rss',rssSources)
+        produceData('rapid',queryStringList)
+    except:
+        pass
     time.sleep(60)
 
 
--- webapp/src/App.js
+++ webapp/src/App.js
@@ -12,7 +12,7 @@ function App() {
     let email = {
         method: 'post',
         contentType: 'application/json',
-        url: "http://192.168.0.1:5000/predict",
+        url: "http://0.0.0.0:1234/predict",
         data: searchItem
       }
       await axios(email).then(res => {
